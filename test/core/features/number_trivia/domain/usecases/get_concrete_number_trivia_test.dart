66cf5cee68a267ffa529101bc974749aa88a2e5f
Ben Schroeter <einsundeins79@gmx.de>
2023-04-06 18:32:44 +0200

    refactoring

--- test/core/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/core/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -27,7 +27,7 @@ void main() {
     when(mockNumberTriviaRepository!.getConcreteNumberTrivia(tNumber))
         .thenAnswer((_) async => Right(tNumberTrivia));
     // act phase
-    final result = await usecase!.execute(number: tNumber);
+    final result = await usecase!(number: tNumber);
     // assert phase
     expect(result, Right(tNumberTrivia));
     verify(mockNumberTriviaRepository!.getConcreteNumberTrivia(tNumber));
